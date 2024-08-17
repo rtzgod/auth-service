ae1321d5fbf7a438c5d54c4f912faec964982054
rtzgod <azimaidynuly1@gmail.com>
2024-08-17 21:20:01 +0500

    schema migrations

---
+++ db/migrations/000001_init.up.sql
@@ -0,0 +1,17 @@
+create table if not exists users (
+    id integer primary key,
+    email text not null unique,
+    pass_hash text not null
+);
+
+create index if not exists idx_email on users(email);
+
+create table if not exists apps (
+    id integer primary key,
+    name text not null unique,
+    secret text not null unique
+);
+
+create table if not exists admins (
+    id integer primary key
+);
