ecc4e362e82291f0015d8d1f3932a47d38ea813f
rtzgod <azimaidynuly@gmail.com>
2024-08-19 23:35:18 +0500

    2 new func

--- internal/repository/postgres/auth.go
+++ internal/repository/postgres/auth.go
@@ -14,22 +14,43 @@ const (
 
 func (r *Repository) SaveUser(ctx context.Context, email string, passHash []byte) (id int64, err error) {
 	const op = "repository.postgres.SaveUser"
+
 	query := fmt.Sprintf("insert into %s(email, pass_hash) values ($1, $2) returning id", usersTable)
+
 	err = r.db.Get(&id, query, email, passHash)
 	if err != nil {
 		return 0, fmt.Errorf("%s: %w", op, err)
 	}
+
 	return id, nil
 }
 
 func (r *Repository) User(ctx context.Context, email string) (user entity.User, err error) {
-	panic("implement me")
-}
+	const op = "repository.postgres.User"
 
-func (r *Repository) IsAdmin(ctx context.Context, userId int64) (isAdmin bool, err error) {
-	panic("implement me")
+	query := fmt.Sprintf("select * from %s where email=$1", usersTable)
+
+	err = r.db.Get(&user, query, email)
+	if err != nil {
+		return entity.User{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return user, nil
 }
 
 func (r *Repository) App(ctx context.Context, appId int) (app entity.App, err error) {
+	const op = "repository.postgres.App"
+
+	query := fmt.Sprintf("select * from %s where id=$1", appsTable)
+
+	err = r.db.Get(&app, query, appId)
+	if err != nil {
+		return entity.App{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return app, nil
+}
+
+func (r *Repository) IsAdmin(ctx context.Context, userId int64) (isAdmin bool, err error) {
 	panic("implement me")
 }
