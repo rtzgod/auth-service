2dbcb8b39eb1aceb05b415ea5616c20499800ae0
rtzgod <azimaidynuly@gmail.com>
2024-08-19 22:52:33 +0500

    saveuser db

--- internal/repository/postgres/auth.go
+++ internal/repository/postgres/auth.go
@@ -2,11 +2,24 @@ package postgres
 
 import (
 	"context"
+	"fmt"
 	"github.com/rtzgod/auth-service/internal/domain/entity"
 )
 
+const (
+	usersTable  = "users"
+	appsTable   = "apps"
+	adminsTable = "admins"
+)
+
 func (r *Repository) SaveUser(ctx context.Context, email string, passHash []byte) (id int64, err error) {
-	return 1, nil
+	const op = "repository.postgres.SaveUser"
+	query := fmt.Sprintf("insert into %s(email, pass_hash) values ($1, $2) returning id", usersTable)
+	err = r.db.Get(&id, query, email, passHash)
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+	return id, nil
 }
 
 func (r *Repository) User(ctx context.Context, email string) (user entity.User, err error) {
